1fbd85cb34aae53a69c23d45f6887ee9834223a6
John Farinas <jfaris09@gmail.com>
2023-11-02 05:47:50 +0800

    Update Vite

--- vite.config.js
+++ vite.config.js
@@ -2,22 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-// export default defineConfig({
-//   base: "/Student-Performance-Analytics/",
-//   plugins: [react()],
-// });
-
-
-
-export default defineConfig(({ command }) => {
-  const config = {
-    plugins: [react()],
-    base: '/',
-  }
-
-  if (command !== 'serve') {
-    config.base = '/Student-Performance-Analytics/'
-  }
-
-  return config
+export default defineConfig({
+  plugins: [react()],
 })
